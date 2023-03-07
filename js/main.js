6874e571b7b20a32ff69a0a6f78488328970cddd
Adolfovi <32370254+Adolfovi@users.noreply.github.com>
2023-03-07 15:58:50 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.querySelector(".main-subpage").addEventListener("mouseover",()=>{
+    document.querySelector(".subpages-header-container").classList.remove("hidden");
+});
+
+document.querySelector(".subpages-header-container").addEventListener("mouseleave",()=>{
+    document.querySelector(".subpages-header-container").classList.add("hidden");
+});
+
+document.querySelector(".subpages-header-container").addEventListener("mouseover",()=>{
+    document.querySelector(".subpages-header-container").classList.remove("hidden");
+});
